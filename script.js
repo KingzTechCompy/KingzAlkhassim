eba97583627ccee14efbea5ea15d03126eceea42
kingz <113271137+Doreally2gd@users.noreply.github.com>
2025-08-18 20:40:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,18 @@ for (var i = 0; i < navLinks.length; i++) {
     menuBtn.style.pointerEvents = "auto";
   });
 }
+
+function createConfetti() {
+      const confetti = document.createElement("div");
+      confetti.classList.add("confetti");
+      confetti.style.left = Math.random() * window.innerWidth + "px";
+      confetti.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+      confetti.style.animationDuration = (Math.random() * 3 + 2) + "s";
+        document.body.appendChild(confetti);
+  
+        setTimeout(() => {
+          confetti.remove();
+        }, 4000);
+      }
+  
+      setInterval(createConfetti, 120);
